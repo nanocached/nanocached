d2b917e2cc3c08d7671c9376f6a007f41849e6d0
Takashi Yamashina <takashi.yamashina@gmail.com>
2026-08-09 09:36:26 +0900

    add tokio

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
+use std::io;
+use tokio::net::TcpListener;
+
 mod cache;
 mod command;
 mod protocol;
 mod response;
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("0.0.0.0:8356").await?;
+
+    loop {
+        let (_stream, address) = listener.accept().await?;
+        println!("accepted connection from {address}");
+    }
 }
