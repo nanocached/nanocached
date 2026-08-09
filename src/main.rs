b877fe63a526a99d0fdf2d10b2be641b5ebc204a
Takashi Yamashina <takashi.yamashina@gmail.com>
2026-08-09 09:45:47 +0900

    implements echo server

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
-use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
 
 mod cache;
 mod command;
@@ -11,7 +12,27 @@ async fn main() -> io::Result<()> {
     let listener = TcpListener::bind("0.0.0.0:8356").await?;
 
     loop {
-        let (_stream, address) = listener.accept().await?;
+        let (stream, address) = listener.accept().await?;
         println!("accepted connection from {address}");
+
+        tokio::spawn(async move {
+            if let Err(error) = handle_connection(stream).await {
+                eprintln!("connection error: {error}");
+            }
+        });
     }
+}
+
+async fn handle_connection(mut stream: TcpStream) -> io::Result<()> {
+    let mut buffer = [0_u8; 1024];
+
+    let bytes_read = stream.read(&mut buffer).await?;
+
+    if bytes_read == 0 {
+        return Ok(());
+    }
+
+    stream.write_all(&buffer[..bytes_read]).await?;
+
+    Ok(())
 }
